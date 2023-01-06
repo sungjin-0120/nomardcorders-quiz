a0945203e610316e40ef38f89363330cdc2e44ef
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-06 09:12:35 +0900

    Update quiz2.py

--- python/quiz2.py
+++ python/quiz2.py
@@ -1,8 +1,8 @@
 ki = True
 while ki:
-    x = int(input('Choose a number:'))
-    y = int(input('Choose another number:'))
-    z = input('choose an operation(+,-,*,/)or Write exit to finish:')
+    x = int(input('Choose a number:\n'))#\n은 띄어쓰기
+    y = int(input('Choose another number:\n'))
+    z = input('choose an operation(+,-,*,/)or Write exit to finish:\n')
     if z == '+':
         k = x+y
         print(f'Result:{k}')
