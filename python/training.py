433d112cb1f14eeaffc8b099aee298b719e4d2da
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-10 01:02:11 +0900

    Upload-py

--- python/training.py
+++ python/training.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from requests import get
 
-
 url = f"https://remoteok.com/remote-python-jobs"
 request = get(url, headers={"User-Agent": "Kimchi"})
 if request.status_code != 200:
@@ -9,32 +8,27 @@ if request.status_code != 200:
 else:
     soup = BeautifulSoup(request.text, "html.parser")
     bodys = soup.find_all('td', class_="company position company_and_position")
+    bodys.pop(0)
+    
     for body in bodys:
+        
         title = body.find('h2', itemprop="title")
         company = body.find('h3', itemprop="name")
         
-        tooltips= body.find('div', class_="location tooltip")
-        print(tooltips)
         anchors = body.find_all('a', itemprop="url", class_="preventLink")
-       
-        """
         for anchor in anchors:
             link = anchor['href']
-            
-            jobs_datas= {
-                    'company': company.string.replace("\n", " "),
-                    'title':title.string.replace("\n", " "),
-                    'region': region.string.replace("\n", " "),
-                    'pay': pay.string.replace("\n", " "),
-                    'link':f"https://remoteok.com/remote-jobs{link}"
-                }
-            print(jobs_datas)
-        """
-            
-
      
-      
+        region = body.find_all('div', class_="location")
+        
+        
+        
+        jobs_data={
+            'company': company.string.strip(),
+            'title': title.string.strip(),
+            'link':f"https://remoteok.com/remote-jobs{link}.strip()"
+        }
+        print(jobs_data)
         
     
-    
-
+        
