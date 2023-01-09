22438bdbdd517d01d5a3f2994b811220b85b65f9
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-09 17:27:37 +0900

    Upload-file

--- python/remote.py
+++ python/remote.py
@@ -8,6 +8,26 @@ if request.status_code != 200:
     print("Can't get jobs.")
 else:
     soup = BeautifulSoup(request.text, "html.parser")
+    bodys = soup.find_all('td', class_="company position company_and_position")
+    for body in bodys:
+        title = body.find('h2', itemprop="title")
+        company = body.find('h3', itemprop="name")
+        region = body.find('div', class_="location tooltip")
+        pay = body.find('div', class_="location tooltip-set")
+        anchors = body.find_all('a', itemprop="url", class_="preventLink")
+        for anchor in anchors:
+            link = anchor['href']
+     
+            jobs_data= {
+                'company': company.string,
+                'title':title,
+                'region':region,
+                'pay':pay,
+                'link':f"https://remoteok.com/remote-jobs{link}"
+            }
+            print(jobs_data)
+      
+        
     
     
 
---
+++ python/training.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+
+url = f"https://remoteok.com/remote-python-jobs"
+request = get(url, headers={"User-Agent": "Kimchi"})
+if request.status_code != 200:
+    print("Can't get jobs.")
+else:
+    soup = BeautifulSoup(request.text, "html.parser")
+    bodys = soup.find_all('td', class_="company position company_and_position")
+    for body in bodys:
+        title = body.find('h2', itemprop="title")
+        company = body.find('h3', itemprop="name")
+        region, pay = body.find('div', class_="location tooltip")
+        anchors = body.find_all('a', itemprop="url", class_="preventLink")
+        for anchor in anchors:
+            link = anchor['href']
+            if region == None or pay == None:
+                jobs_datas= {
+                    'company': company.string.replace("\n", " ").replace(","," "),
+                    'title':title.string.replace("\n", " "),
+                    'region': 'None',
+                    'pay': 'None',
+                    'link':f"https://remoteok.com/remote-jobs{link}"
+                }
+            else:
+                jobs_datas= {
+                    'company': company.string.replace("\n", " "),
+                    'title':title.string.replace("\n", " "),
+                    'region': region.string.replace("\n", " "),
+                    'pay': pay.string.replace("\n", " "),
+                    'link':f"https://remoteok.com/remote-jobs{link}"
+                }
+            print(jobs_datas)
+            
+
+     
+      
+        
+    
+    
+
