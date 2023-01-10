9e454839da71693e86c991b1b688f58ba4027b07
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-10 11:13:10 +0900

    Update training.py

--- python/training.py
+++ python/training.py
@@ -6,6 +6,7 @@ request = get(url, headers={"User-Agent": "Kimchi"})
 if request.status_code != 200:
     print("Can't get jobs.")
 else:
+    get=[]
     soup = BeautifulSoup(request.text, "html.parser")
     bodys = soup.find_all('td', class_="company position company_and_position")
     bodys.pop(0)
@@ -17,20 +18,23 @@ else:
         anchors = body.find_all('a', itemprop="url", class_="preventLink")
         for anchor in anchors:
             link = anchor['href']
+        location_2 = body.find('div', class_="location")
         locations = body.find_all('div', class_="location")
         for location in locations:
-            region= location[1]
-            print(region)
-        
-        """
+            pay = location
         jobs_data={
             'company': company.string.strip(),
             'title': title.string.strip(),
-            'location':location.string.strip(),
+            'location':location_2.string.strip(),
+            'pay':pay.string.strip(),
             'link':f"https://remoteok.com/remote-jobs{link}"
         }
         print(jobs_data)
-        """
+        
+        
+        
+        
+       
         
         
         
