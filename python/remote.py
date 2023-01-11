3c758037c96be283cb67329e138c93753bb13125
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-11 10:18:58 +0900

    Update remote.py

--- python/remote.py
+++ python/remote.py
@@ -1,34 +1,36 @@
 from bs4 import BeautifulSoup
 from requests import get
 
-url = f"https://remoteok.com/remote-python-jobs"
-request = get(url, headers={"User-Agent": "Kimchi"})
-if request.status_code != 200:
-    print("Can't get jobs.")
-else:
-    soup = BeautifulSoup(request.text, "html.parser")
-    bodys = soup.find_all('td', class_="company position company_and_position")
-    bodys.pop(0)
-    for body in bodys:
-        
-        title = body.find('h2', itemprop="title")
-        company = body.find('h3', itemprop="name")
-        
-        anchors = body.find_all('a', itemprop="url", class_="preventLink")
-        for anchor in anchors:
-            link = anchor['href']
-        location_2 = body.find('div', class_="location")
-        locations = body.find_all('div', class_="location")
-        for location in locations:
-            pay = location
-        jobs_data={
-            'company': company.string.strip(),
-            'title': title.string.strip(),
-            'location':location_2.string.strip(),
-            'pay':pay.string.strip(),
-            'link':f"https://remoteok.com/remote-jobs{link}"
-        }
-        print(jobs_data)
+def extract_remote(term):
+    url = f"https://remoteok.com/remote-{term}-jobs"
+    request = get(url, headers={"User-Agent": "Kimchi"})
+    if request.status_code != 200:
+        print("Can't get jobs.")
+    else:
+        soup = BeautifulSoup(request.text, "html.parser")
+        bodys = soup.find_all('td', class_="company position company_and_position")
+        bodys.pop(0)
+        for body in bodys:
+            
+            title = body.find('h2', itemprop="title")
+            company = body.find('h3', itemprop="name")
+            
+            anchors = body.find_all('a', itemprop="url", class_="preventLink")
+            for anchor in anchors:
+                link = anchor['href']
+            location_2 = body.find('div', class_="location")
+            locations = body.find_all('div', class_="location")
+            for location in locations:
+                pay = location
+            jobs_data={
+                'company': company.string.strip(),
+                'title': title.string.strip(),
+                'location':location_2.string.strip(),
+                'pay':pay.string.strip(),
+                'link':f"https://remoteok.com/remote-jobs{link}"
+            }
+            print(jobs_data)
+extract_remote('java')
         
         
         
